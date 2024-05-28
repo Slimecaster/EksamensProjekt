0014c270d2cfda13cb83c57330485d5c35a77229
amalieasborg <amalieasborg@gmail.com>
2024-05-28 14:15:10 +0200

    ...

--- src/main/java/com/example/eksamensprojekt/Config/SecurityConfig.java
+++ src/main/java/com/example/eksamensprojekt/Config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.eksamensprojekt.Config;
 
-import com.example.eksamensprojekt.AuthSuccHandler;
 import com.example.eksamensprojekt.Service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,20 +7,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.util.AntPathMatcher;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig
 {
+
     @Autowired
     private CustomUserDetailsService userDetailService;
 
