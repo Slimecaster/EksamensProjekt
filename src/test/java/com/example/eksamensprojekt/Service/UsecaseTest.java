639b30d59bbafc60ca587f166fe9c8ab822eb341
amalieasborg <amalieasborg@gmail.com>
2024-05-30 09:13:11 +0200

    To unittestforsøg til calculateBMR, der ikke består testen.

--- src/test/java/com/example/eksamensprojekt/Service/UsecaseTest.java
+++ src/test/java/com/example/eksamensprojekt/Service/UsecaseTest.java
@@ -29,38 +29,35 @@ class UsecaseTest {
     @InjectMocks
     private DBcontroller dbController;
 
+    @InjectMocks
     Usecase usecase=new Usecase();
-/*
+
+
     @Test
-    public void testCalculateBMRForMale() {
-        // Creating a male test user with weight 70kg, height 170cm, age 30, and gender 0 for male
-        MyUser maleUser = new MyUser(1L,"test","testesen","1234","test@mail.com","1234 5678",70,170,30,0,1,2,"Admin");
+    void calculateBMRForFemale(){
+        MyUser user = new MyUser(1L,"Fname1","Sname1","Password1","email1@gmail.com","11 22 33 44",84,174,42,1,2,0,"User");
 
-        when(jdbcTemplate.queryForObject(any(String.class), any(Object[].class), any(RowMapper.class)))
-                .thenReturn(maleUser);
+        Double expectedBMR=10 * 84 + 6.25 * 174 - 5 * 42 - 161;
+        Double actualBMR= usecase.calculateBMR("email1@gmail.com");
 
-        // Calculating BMR for male user
-        Double expectedBMR = 10 * 70 + 6.25 * 170 - 5 * 30 + 5;
-        Double actualBMR = usecase.calculateBMR(maleUser.getEmail());
-
-        // Asserting that the calculated BMR matches the expected value
         assertEquals(expectedBMR, actualBMR);
     }
 
     @Test
-    public void testCalculateBMRForFemale() {
-        // Creating a female user with weight 60kg, height 160cm, age 25, and gender 1 for female/not male
-        MyUser femaleUser = new MyUser(1L,"testk","testesenk","1234","test@mail.com","1234 5678",60,160,25,1,1,2,"Admin");
+    void calculateBMRForMale(){
+        MyUser user = new MyUser(1L,"Fname1","Sname1","Password1","email1@gmail.com","11 22 33 44",84,174,42,0,2,0,"User");
 
-        // Calculating BMR for female user
-        Double expectedBMR = 10 * 60 + 6.25 * 160 - 5 * 25 - 161;
-        Double actualBMR = usecase.calculateBMR(femaleUser.getEmail());
+        Double expectedBMR=10 * 84 + 6.25 * 174 - 5 * 42 + 5;
+        Double actualBMR= usecase.calculateBMR("email1@gmail.com");
 
-        // Asserting that the calculated BMR matches the expected value
         assertEquals(expectedBMR, actualBMR);
     }
 
- */
+
+
+
+
+
 
 
 
