14987283ef3446bf37cdea4c5333850d36e21eb2
amalieasborg <amalieasborg@gmail.com>
2024-05-30 09:23:25 +0200

    Opdaterede informationen i MyUser-objektet i begge unittest, så de passede med konstruktøren i MyUser-klassen.

--- src/test/java/com/example/eksamensprojekt/Repository/DBcontrollerTest.java
+++ src/test/java/com/example/eksamensprojekt/Repository/DBcontrollerTest.java
@@ -1,6 +1,7 @@
 package com.example.eksamensprojekt.Repository;
 
 import com.example.eksamensprojekt.Model.MyUser;
+import com.example.eksamensprojekt.Model.Subscription;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,16 +21,18 @@ class DBcontrollerTest {
     @InjectMocks
     private DBcontroller dbController;
 
+    Subscription subscription=new Subscription();
+
 
     @Test
     void createUpdateUser(){
-        MyUser user = new MyUser(1L,"Fname1","Sname1","Password1","email1@gmail.com","11 22 33 44",84,174,42,1,2,0,"User");
+        MyUser user = new MyUser(1L,"Fname1","Sname1","Password1","email1@gmail.com","11 22 33 44","User",84,174,42,1,2,0,subscription);
         dbController.createUpdateUser(user);
     }
 
     @Test
     void deleteUserByEmail(){
-        MyUser user = new MyUser(1L,"Fname1","Sname1","Password1","email1@gmail.com","11 22 33 44",84,174,42,1,2,0,"User");
+        MyUser user = new MyUser(1L,"Fname1","Sname1","Password1","email1@gmail.com","11 22 33 44","User",84,174,42,1,2,0,subscription);
         dbController.deleteUserByEmail("email1@gmail.com");
     }
 
